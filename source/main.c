beb48ae98acdd35a6786679154361af9efff3e0d
richardwaifro <77700999+richardwaifro@users.noreply.github.com>
2021-06-08 22:00:13 +0600

    Add files via upload

---
+++ source/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "pp4m/pp4m.h"
+#include "pp4m/pp4m_image.h"
+#include "pp4m/pp4m.h"
+
+SDL_Window *global_window = NULL;
+SDL_Renderer *global_renderer = NULL;
+
+int main (int argc, char *argv[]) {
+
+    global_renderer = pp4m_Init(global_window, "scacchi", 1270, 720);
+
+    for(;;) {}
+
+    pp4m_Quit();
+
+    return 0;
+}
