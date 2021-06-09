da7fbd21737d32700f8050da5d1e66705d9f7cd2
richardwaifro <77700999+richardwaifro@users.noreply.github.com>
2021-06-09 21:01:52 +0200

    Add files via upload

--- source/main.c
+++ source/main.c
@@ -8,24 +8,25 @@
 #include "main.h"
 #include "game.h"
 
-SDL_Window *global_window;
+SDL_Window *global_window = NULL;
 SDL_Renderer *global_renderer;
 
 int main (int argc, char *argv[]) {
 
     global_renderer = pp4m_Init(global_window, "scacchi", 1280, 720);
-
+    
     PP4M_SDL background;
     background.texture = pp4m_DRAW_TextureRect(global_renderer, PP4M_GREY_DARK, &background.rect, 1280, 720, 1, 1);
 
     SDL_RenderClear(global_renderer);
     SDL_RenderCopy(global_renderer, background.texture, NULL, NULL);
+    SDL_RenderPresent(global_renderer);
 
     GAME_CreateChessboard_RightTiles();
     GAME_CreateChessboard_LeftTiles();
 
     PP4M_SDL BlackKing;
-    BlackKing.texture = pp4m_IMG_ImageToRenderer(global_renderer, NULL, "C:/Users/michele.SPAZIOTEMPO/Desktop/Programmi/Progetti/Scacchi/resources/re.png", &BlackKing.rect, 490, 160, 50, 50);
+    BlackKing.texture = pp4m_IMG_ImageToRenderer(global_renderer, NULL, "/home/waifro/Scrivania/projects/Scacchi-main/resources/bianco_re.png", &BlackKing.rect, 490, 160, 50, 50);
 
     SDL_RenderCopy(global_renderer, BlackKing.texture, NULL, &BlackKing.rect);
     SDL_RenderPresent(global_renderer);
--- source/pp4m/pp4m_draw.c
+++ source/pp4m/pp4m_draw.c
@@ -5,28 +5,24 @@
 
 SDL_Texture *pp4m_DRAW_TextureRect(SDL_Renderer *renderer, SDL_Color color, SDL_Rect *rect, float x, float y, float w, float h) {
 
-    SDL_Surface surface = {0};
+    SDL_Surface *surface = NULL;
     SDL_Texture *texture = NULL;
 
-    if (sizeof(surface) > 1) {
-        SDL_FreeSurface(&surface);
-    }
-
-    memcpy(&surface, (void*)SDL_CreateRGBSurface(0, w, h, 32, 0, 0, 0, 0), sizeof(surface));
+    surface = SDL_CreateRGBSurface(0, w, h, 32, 0, 0, 0, 0);
 
     rect->x = 0;
     rect->y = 0;
     rect->w = (int)w;
     rect->h = (int)h;
 
-    SDL_FillRect(&surface, rect, SDL_MapRGB((void*)surface.format, color.r, color.g, color.b));
+    SDL_FillRect(surface, rect, SDL_MapRGB(surface->format, color.r, color.g, color.b));
 
     rect->x = (int)x;
     rect->y = (int)y;
 
-    texture = SDL_CreateTextureFromSurface(renderer, &surface);
+    texture = SDL_CreateTextureFromSurface(renderer, surface);
 
-    SDL_FreeSurface(&surface);
+    SDL_FreeSurface(surface);
     return texture;
 
 }
