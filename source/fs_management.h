38cea469def888fdb80b1833d43ff71ce720c759
richardwaifro <waifro@protonmail.com>
2021-08-01 12:22:57 +0200

    commit inizialization

--- source/fs_management.h
+++
@@ -1,6 +0,0 @@
-#ifndef _FS_MANAGEMENT_H
-#define _FS_MANAGEMENT_H
-
-int FS_RecordPieceMovement(int old, int foo);
-
-#endif // _FS_MANAGEMENT_H
--- source/logic.h
+++
@@ -1,8 +0,0 @@
-#ifndef _LOGIC_H
-#define _LOGIC_H
-
-#include <stdbool.h>
-
-bool LOGIC_ControlGameplayChessboard(int pos);
-
-#endif // _LOGIC_H
