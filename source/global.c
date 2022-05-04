b72984e6b5a3d4dc46d67217ef4af6e51b4c1fee
waifro <waifro.bio@gmail.com>
2022-05-04 23:01:19 +0200

    added *GLOBAL_HookArray... this will be used to pass textures to the new loop (to keep his background) while theres a popup

--- source/global.c
+++ source/global.c
@@ -1,4 +1,5 @@
 // third-party libraries
+#include <stdlib.h>
 #include <SDL2/SDL.h>
 
 // local libraries
@@ -11,3 +12,36 @@ SDL_Renderer *glo_render = NULL;
 
 int glo_screen_w = 1280;
 int glo_screen_h = 720;
+
+// functions
+size_t GLOBAL_HookArray_Size(uintptr_t **array) {
+    for (int n = 0; n < BUF_MAX; n++)
+        if (array[n] == NULL) return (n);
+    return -1;
+}
+
+size_t GLOBAL_HookArray_Reference(uintptr_t **array, uintptr_t *ptr) {
+    size_t index = GLOBAL_HookArray_Size(array);
+    if (index == -1) return -1;
+
+    array = realloc(array, index+1);
+    array[index] = ptr;
+
+    return (index);
+}
+
+size_t GLOBAL_HookArray_Dereference(uintptr_t **array) {
+    size_t index = GLOBAL_HookArray_Size(array);
+    if (index == -1) return -1;
+
+    array[index-1] = NULL;
+    array = realloc(array, index-1); // truncates size of array
+
+    return (index-1);
+}
+
+uintptr_t **GLOBAL_HookArray_Init(void) {
+    uintptr_t **array = malloc(sizeof(void*) * 1);
+    array[0] = NULL;
+    return (array);
+}
--- source/global.h
+++ source/global.h
@@ -2,10 +2,13 @@
 #define GLOBAL_H
 
 // third-party libraries
+#include <stdlib.h>
 #include <SDL2/SDL.h>
 
 // definitions
 #define SIZEOF(a) sizeof(a)/sizeof(*a)
+#define BUF_MAX 0x10000
+    /* 65536 bytes */
 
 // declared extern variables
 extern SDL_Window *glo_window;
@@ -13,5 +16,11 @@ extern SDL_Renderer *glo_render;
 
 extern int glo_screen_w;
 extern int glo_screen_h;
+
+// prototypes
+uintptr_t **GLOBAL_HookArray_Init(void);
+size_t GLOBAL_HookArray_Reference(uintptr_t **array, void *ptr);
+size_t GLOBAL_HookArray_Dereference(uintptr_t **array);
+size_t GLOBAL_HookArray_Size(uintptr_t **array);
 
 #endif
