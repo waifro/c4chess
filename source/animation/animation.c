7e5c8f53dfba34fd2f6d7ec96f82bdbee3322baa
waifro <waifro.bio@gmail.com>
2022-05-23 17:06:07 +0600

    fixed few problems (fps independece)

--- source/animation/animation.c
+++ source/animation/animation.c
@@ -2,10 +2,12 @@
 
 #include <time.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <SDL2/SDL.h>
 
-int ANIM_UpdateRect(float deltaTime, int quantity, SDL_Rect *src, SDL_Rect dest) {
+int ANIM_UpdateRect(float deltaTime, int quantity, float time, SDL_Rect *src, SDL_Rect dest) {
 
+    /*
     if (src->x != dest.x || src->y != dest.y) {
 
         if (src->x < dest.x) src->x += quantity * (int)deltaTime / CLOCKS_PER_SEC;
@@ -16,6 +18,54 @@ int ANIM_UpdateRect(float deltaTime, int quantity, SDL_Rect *src, SDL_Rect dest)
         return -1;
 
     }
+    */
+
+    static SDL_Rect src_bak;
+    static bool door = false;
+    if (door == false) {
+        src_bak = *src;
+        door = true;
+    }
+
+    static float x1 = 0.0f;
+    static float y1 = 0.0f;
+    static int n = 0;
+
+    if (src->x != dest.x || src->y != dest.y) {
+
+        if (src->x > dest.x || src->x < dest.x) {
+            x1 += deltaTime / CLOCKS_PER_SEC + ((src_bak.x > dest.x ? src_bak.x - dest.x : src_bak.x + dest.x) / time);
+        }
+
+        if (src->y > dest.y || src->y < dest.y) {
+            y1 += deltaTime / CLOCKS_PER_SEC + ((src_bak.y > dest.y ? src_bak.y - dest.y : src_bak.y + dest.y) / time);
+        }
+
+        printf("%d %f %d %f: %d\n", (int)x1, x1, (int)y1, y1, n);
+
+        if (x1 > 1.0f) {
+            if (src->x < dest.x) src->x += (int)x1;
+            else if (src->x > dest.x) src->x -= (int)x1;
+            x1 = 0.0f;
+        }
+
+        if (y1 > 1.0f) {
+            if (src->y < dest.y) src->y += (int)y1;
+            else if (src->y > dest.y) src->y -= (int)y1;
+            y1 = 0.0f;
+        }
+
+
+        n++;
+        return -1;
+    }
+
+    src_bak.x = 0;
+    src_bak.y = 0;
+    src_bak.w = 0;
+    src_bak.h = 0;
+    door = false;
+    n = 0;
 
     return 0;
 }
--- source/version.h
+++ source/version.h
@@ -13,18 +13,18 @@
 	
 	//Standard Version Type
 	#define MAJOR  0
-	#define MINOR  0
-	#define BUILD  56
-	#define REVISION  344
+	#define MINOR  1
+	#define BUILD  117
+	#define REVISION  661
 	
 	//Miscellaneous Version Types
-	#define BUILDS_COUNT  65
-	#define RC_FILEVERSION 0,0,56,344
-	#define RC_FILEVERSION_STRING "0, 0, 56, 344\0"
-	#define FULLVERSION_STRING  "0.0.56.344"
+	#define BUILDS_COUNT  140
+	#define RC_FILEVERSION 0,1,117,661
+	#define RC_FILEVERSION_STRING "0, 1, 117, 661\0"
+	#define FULLVERSION_STRING  "0.1.117.661"
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	#define BUILD_HISTORY  56
+	#define BUILD_HISTORY  17
 	
 
 #endif //VERSION_H
