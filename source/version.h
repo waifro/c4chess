e3eb8ddd86d4e3fbd8dceb421ae4bb5ca78a0d50
waifro <waifro.bio@gmail.com>
2022-05-28 13:48:20 +0200

    updated ver to 0.0a -> 0.1a

--- source/version.h
+++ source/version.h
@@ -6,25 +6,25 @@
 	#define MONTH "05"
 	#define YEAR "2022"
 	#define UBUNTU_VERSION_STYLE  "22.05"
-	
+
 	//Software Status
 	#define STATUS  ""
 	#define STATUS_SHORT  ""
-	
+
 	//Standard Version Type
 	#define MAJOR  0
-	#define MINOR  0
+	#define MINOR  1
 	#define BUILD  23
 	#define REVISION  156
-	
+
 	//Miscellaneous Version Types
 	#define BUILDS_COUNT  24
 	#define RC_FILEVERSION 0,0,23,156
 	#define RC_FILEVERSION_STRING "0, 0, 23, 156\0"
 	#define FULLVERSION_STRING  "0.0.23.156"
-	
+
 	//These values are to keep track of your versioning state, don't modify them.
 	#define BUILD_HISTORY  23
-	
+
 
 #endif //VERSION_H
