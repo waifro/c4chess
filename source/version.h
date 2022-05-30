1d153190dab6abb6054124c92f437cbd870a9b1d
waifro <waifro.bio@gmail.com>
2022-05-30 18:19:51 +0600

    should be fixed autovers.

--- source/version.h
+++ source/version.h
@@ -13,15 +13,15 @@
 	
 	//Standard Version Type
 	#define MAJOR  0
-	#define MINOR  0
+	#define MINOR  1
 	#define BUILD  64
 	#define REVISION  375
 	
 	//Miscellaneous Version Types
-	#define BUILDS_COUNT  74
-	#define RC_FILEVERSION 0,0,64,375
-	#define RC_FILEVERSION_STRING "0, 0, 64, 375\0"
-	#define FULLVERSION_STRING  "0.0.64.375"
+	#define BUILDS_COUNT  75
+	#define RC_FILEVERSION 0,1,64,375
+	#define RC_FILEVERSION_STRING "0, 1, 64, 375\0"
+	#define FULLVERSION_STRING  "0.1.64.375"
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	#define BUILD_HISTORY  64
