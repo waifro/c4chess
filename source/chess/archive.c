d5165eb88fba17d1c611ebf9486c45edffe4612e
waifro <waifro.bio@gmail.com>
2022-06-01 16:54:20 +0600

    few changes on archive

--- source/chess/archive.c
+++ source/chess/archive.c
@@ -7,22 +7,20 @@
 #include "archive.h"
 
 char *glo_chess_archive_flow = NULL;
+char *glo_chess_archive_flow_mark = "kbnrq";
 
 void ARCHIVE_Notation_EnrollMove(CHESS_CORE_TILE *chess_tile, int position_old, int position_new) {
 
     if (!glo_chess_archive_flow)
         glo_chess_archive_flow = malloc(sizeof(char*));
 
-    ARCHIVE_NOTATION_PIECE piece;
-    piece = ARCHIVE_Redirect_StateMove(chess_tile, position_new);
+    ARCHIVE_NOTATION_PIECE piece_ntt;
+    piece_ntt = ARCHIVE_Redirect_StateMove(chess_tile, position_new);
 
-    if (piece == NONE_A) ;
-    if (piece == KING_A) ;
-    if (piece == PAWN_A) ;
-    if (piece == BISHOP_A) ;
-    if (piece == KNIGHT_A) ;
-    if (piece == ROOK_A) ;
-    if (piece == QUEEN_A) ;
+    char buffer[16]; int ind = 0;
+
+    if (piece_ntt != NONE_A)
+        buffer[ind] = glo_chess_archive_flow_mark[piece_ntt];
 
     return;
 }
@@ -31,7 +29,7 @@ void ARCHIVE_Notation_EnrollMove(CHESS_CORE_TILE *chess_tile, int position_old, 
 ARCHIVE_NOTATION_PIECE ARCHIVE_Redirect_StateMove(CHESS_CORE_TILE *chess_tile, int slot) {
 
     if (CHESS_Redirect_EnumKing(chess_tile, slot) == 0) return KING_A;
-    else if (CHESS_Redirect_EnumPawn(chess_tile, slot) == 0) return PAWN_A;
+    else if (CHESS_Redirect_EnumPawn(chess_tile, slot) == 0) return NONE_A;
     else if (CHESS_Redirect_EnumBishop(chess_tile, slot) == 0) return BISHOP_A;
     else if (CHESS_Redirect_EnumKnight(chess_tile, slot) == 0) return KNIGHT_A;
     else if (CHESS_Redirect_EnumRook(chess_tile, slot) == 0) return ROOK_A;
--- source/chess/archive.h
+++ source/chess/archive.h
@@ -2,17 +2,16 @@
 #define ARCHIVE_H
 
 typedef enum {
-    NONE_A,
     KING_A,
-    PAWN_A,
     BISHOP_A,
     KNIGHT_A,
     ROOK_A,
-    QUEEN_A
-
+    QUEEN_A,
+    NONE_A
 } ARCHIVE_NOTATION_PIECE;
 
 extern char *glo_chess_archive_flow;
+extern char *glo_chess_archive_flow_mark;
 
 void ARCHIVE_Notation_EnrollMove(CHESS_CORE_TILE *chess_tile, int position_old, int position_new);
 ARCHIVE_NOTATION_PIECE ARCHIVE_Redirect_StateMove(CHESS_CORE_TILE *chess_tile, int slot);
