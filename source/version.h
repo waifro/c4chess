45d3d76559830a127f0416818a4e5eaf62a2da3a
waifro <waifro.bio@gmail.com>
2022-06-03 17:27:00 +0600

    few minor changes

--- source/version.h
+++ source/version.h
@@ -14,17 +14,17 @@
 	//Standard Version Type
 	#define MAJOR  0
 	#define MINOR  0
-	#define BUILD  37
-	#define REVISION  200
+	#define BUILD  38
+	#define REVISION  204
 	
 	//Miscellaneous Version Types
-	#define BUILDS_COUNT  40
-	#define RC_FILEVERSION 0,0,37,200
-	#define RC_FILEVERSION_STRING "0, 0, 37, 200\0"
-	#define FULLVERSION_STRING  "0.0.37.200"
+	#define BUILDS_COUNT  41
+	#define RC_FILEVERSION 0,0,38,204
+	#define RC_FILEVERSION_STRING "0, 0, 38, 204\0"
+	#define FULLVERSION_STRING  "0.0.38.204"
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	#define BUILD_HISTORY  37
+	#define BUILD_HISTORY  38
 	
 
 #endif //VERSION_H
