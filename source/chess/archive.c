ef34a76e7daa6ae61c9b90dabb70dda7d88eba1f
waifro <waifro.bio@gmail.com>
2022-06-05 12:07:13 +0200

    fixed archive -> Notation_RecordMove

--- source/chess/archive.c
+++ source/chess/archive.c
@@ -6,81 +6,58 @@
 #include "chess.h"
 #include "archive.h"
 
-char *glo_chess_archive_record;
 int glo_chess_archive_tmp_tile[2];
-void *glo_chess_archive_tmp_ptr = NULL;
+void *glo_chess_archive_tmp_ptr;
 
-void ARCHIVE_Notation_RecordMove(CHESS_CORE_TILE *chess_tile, bool king_uatk, void *ptr, int slot_old, int slot_new) {
+void ARCHIVE_Notation_RecordMove(CHESS_CORE_TILE *chess_tile, bool king_uatk, void *ptr, int tile_old, int tile_new) {
 
-    /*
-    if (!slot_old || !slot_new) return;
+    if (!tile_old) return;
 
-    char *piece_idnt = "KBNRQ";
+    char *notation_piece_char = "KBNRQ";
+    char buf[6]; char foo[3];
+    char tag[3]; sprintf(tag, "%c%d", chess_tile[tile_new].tag.col, chess_tile[tile_new].tag.row);
 
-    char buf[6] = {0};
-    char tag[3] = {0};
+    ARCHIVE_NOTATION_PIECE notation_piece = ARCHIVE_Redirect_StateMove(chess_tile, tile_new);
 
-    int piece_ntt = ARCHIVE_Redirect_StateMove(chess_tile, slot_old);
+    switch(notation_piece) {
+        case PAWN_A:
+            if (ptr == NULL) break;
+            foo[0] = chess_tile[tile_old].tag.col;
+            foo[1] = 'x';
+            break;
 
+        case NONE_A:
+            break;
 
-    if (piece_ntt != NONE_A && piece_ntt != PAWN_A) {
-        buf[0] = piece_idnt[piece_ntt];
-        if (ptr != NULL) buf[1] = 'x';
-    } else if (piece_ntt == PAWN_A) {
-        if (ptr != NULL) {
-            buf[0] = chess_tile[slot_old].tag.col;
-            buf[1] = 'x';
-        } else buf[0] = '\0';
+        default:
+            if (ptr != NULL) {
+                foo[0] = notation_piece_char[notation_piece];
+                foo[1] = 'x';
+
+            } else {
+                foo[0] = notation_piece_char[notation_piece];
+                foo[1] = '\0';
+            }
+            break;
     }
 
-    sprintf(tag, "%c%d", chess_tile[slot_new].tag.col, chess_tile[slot_new].tag.row);
-
-    printf("\nbuf [%s]\n", buf);
-    printf("buf [%s]\n", tag);
+    if (strlen(foo) > 0) strcpy(buf, foo);
+    else buf[0] = '\0';
 
     strcat(buf, tag);
 
     if (king_uatk == true) strcat(buf, "+ ");
-    else strcat(buf, " ");
+    else { buf[strlen(buf)] = ' '; buf[strlen(buf)] = '\0'; }
 
-    printf("buf [%s]\n", buf);
+    printf("record[%s]\n", buf);
 
-    char notation[16];
-    ARCHIVE_NOTATION_PIECE piece_ntt;
-    char buffer[3] = ""; int ind = 0;
-    CHESS_CORE_TILE_TAG tag = chess_tile[slot_new].tag;
-
-    piece_ntt = ARCHIVE_Redirect_StateMove(chess_tile, slot_old);
-
-    if (piece_ntt != NONE_A && piece_ntt != PAWN_A) {
-        buffer[ind++] = piece_idnt[piece_ntt];
-        if (piece != NULL) buffer[ind] = 'x';
-    }
-
-    else if (piece_ntt == PAWN_A && piece != NULL) {
-        buffer[ind++] = chess_tile[slot_old].tag.col;
-        buffer[ind] = 'x';
-    }
-
-    sprintf(notation, "%s%c%d", buffer, tag.col, tag.row);
-
-    if (king_uatk == true) strcat(notation, "+ ");
-    else strcat(notation, " ");
-
-    printf("buf [%s]\n", notation);
-
-    glo_chess_archive_record = realloc(glo_chess_archive_record, sizeof(glo_chess_archive_record) + strlen(buf));
-    strncat(glo_chess_archive_record, buf, strlen(buf));
-
-    printf("archive: [%s]\n", glo_chess_archive_record);
-    */
     return;
 }
 
 
 ARCHIVE_NOTATION_PIECE ARCHIVE_Redirect_StateMove(CHESS_CORE_TILE *chess_tile, int slot) {
 
-    if (chess_tile[slot].piece == NULL) return NONE_A;
+    if (chess_tile[slot].piece == NULL) return -1;
 
     if (CHESS_Redirect_EnumKing(chess_tile, slot) == 0) return KING_A;
     else if (CHESS_Redirect_EnumBishop(chess_tile, slot) == 0) return BISHOP_A;
@@ -88,6 +65,5 @@ ARCHIVE_NOTATION_PIECE ARCHIVE_Redirect_StateMove(CHESS_CORE_TILE *chess_tile, i
     else if (CHESS_Redirect_EnumRook(chess_tile, slot) == 0) return ROOK_A;
     else if (CHESS_Redirect_EnumQueen(chess_tile, slot) == 0) return QUEEN_A;
     else if (CHESS_Redirect_EnumPawn(chess_tile, slot) == 0) return PAWN_A;
-
-    return -1;
+    else return NONE_A;
 }
--- source/chess/archive.h
+++ source/chess/archive.h
@@ -3,12 +3,11 @@
 
 #include "core.h"
 
-extern char *glo_chess_archive_record;
 extern int glo_chess_archive_tmp_tile[2];
 extern void *glo_chess_archive_tmp_ptr;
 
 typedef enum {
-    KING_A,
+    KING_A = 0,
     BISHOP_A,
     KNIGHT_A,
     ROOK_A,
@@ -17,7 +16,7 @@ typedef enum {
     NONE_A
 } ARCHIVE_NOTATION_PIECE;
 
-void ARCHIVE_Notation_RecordMove(CHESS_CORE_TILE *chess_tile, bool king_uatk, void *ptr, int slot_old, int slot_new);
+void ARCHIVE_Notation_RecordMove(CHESS_CORE_TILE *chess_tile, bool king_uatk, void *ptr, int tile_old, int tile_new);
 ARCHIVE_NOTATION_PIECE ARCHIVE_Redirect_StateMove(CHESS_CORE_TILE *chess_tile, int slot);
 
 #endif // ARCHIVE_H
