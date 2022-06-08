ccab47f05922b08c7bf4541bfb87103028590371
waifro <waifro.bio@gmail.com>
2022-06-08 18:01:47 +0600

    fixed king on defending

--- source/chess/chess.c
+++ source/chess/chess.c
@@ -212,6 +212,10 @@ int CHESS_PiecePattern_King(CHESS_CORE_TILE *core_tile, int tile, CHESS_CORE_PLA
 
         if (result == -1) continue;
 
+        if (glo_chess_event_layer[result] == false)
+            core_tile[tile].piece->range[result] = true;
+
+        /*
         if (core_tile[result].piece == NULL) {
             if (glo_chess_event_layer[result] == false)
                 core_tile[tile].piece->range[result] = true;
@@ -221,6 +225,7 @@ int CHESS_PiecePattern_King(CHESS_CORE_TILE *core_tile, int tile, CHESS_CORE_PLA
             if (glo_chess_event_layer[result] == false)
                 core_tile[tile].piece->range[result] = true;
         }
+        */
     }
 
     return 0;
