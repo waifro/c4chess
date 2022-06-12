0fd0e56ab4a981f76a90cfd8530d55187b051efc
waifro <waifro.bio@gmail.com>
2022-06-12 17:49:06 +0200

    testing again

--- source/chess/core.c
+++ source/chess/core.c
@@ -216,7 +216,7 @@ int CORE_NET_SendRoomState(net_sockrid_t *sockrid, int *running, CHESS_CORE_PLAY
     if (*running == -2) {
         char buf[256];
         sprintf(buf, "%d %d %d", sockrid->roomId, *tile_old, *tile_new);
-        write(*sockrid->socket, buf, strlen(buf));
+        send(*sockrid->socket, buf, strlen(buf), 0);
     }
 
     return 0;
