680f514ecc089b1c21e0a5299874ea9d624bbba4
waifro <waifro.bio@gmail.com>
2022-06-15 16:30:52 +0600

    windows Makefile update

--- source/version.h
+++ source/version.h
@@ -14,17 +14,17 @@
 	//Standard Version Type
 	#define MAJOR  0
 	#define MINOR  0
-	#define BUILD  15
-	#define REVISION  78
+	#define BUILD  16
+	#define REVISION  88
 	
 	//Miscellaneous Version Types
-	#define BUILDS_COUNT  17
-	#define RC_FILEVERSION 0,0,15,78
-	#define RC_FILEVERSION_STRING "0, 0, 15, 78\0"
-	#define FULLVERSION_STRING  "0.0.15.78"
+	#define BUILDS_COUNT  18
+	#define RC_FILEVERSION 0,0,16,88
+	#define RC_FILEVERSION_STRING "0, 0, 16, 88\0"
+	#define FULLVERSION_STRING  "0.0.16.88"
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	#define BUILD_HISTORY  15
+	#define BUILD_HISTORY  16
 	
 
 #endif //VERSION_H
