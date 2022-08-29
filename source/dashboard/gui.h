f815b6d9f87dd62e5f28fbedd62ed5b419f3d6ff
waifro <waifro.bio@gmail.com>
2022-08-29 19:10:43 +0200

    removed old code

--- source/dashboard/gui.h
+++ source/dashboard/gui.h
@@ -22,6 +22,5 @@ int GUI_HookLink_Render(PP4M_HOOK *link);
 void GUI_HookList_Quit(PP4M_HOOK *hook_list);
 
 int GUI_HookLink_Update(PP4M_HOOK *link, PP4M_INPUT_POS input, char **buffer, int key, int *code);
-int GUI_HookList_Update(PP4M_HOOK *hook_list, PP4M_INPUT_POS input, char **buffer, int key, int *code);
 
 #endif
